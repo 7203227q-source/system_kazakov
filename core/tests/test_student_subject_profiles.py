f222d9d421dca09f40767f4bce5ffd7eac9b7096
7203227q-source <7203227q-source@users.noreply.github.com>
2026-05-12 15:13:38 +0000

    feat: add learning settings link to student sidebar

--- core/tests/test_student_subject_profiles.py
+++ core/tests/test_student_subject_profiles.py
@@ -11,3 +11,12 @@ class StudentLearningSettingsPageTests(TestCase):
         res = self.client.get(reverse("student_learning_settings"))
         self.assertEqual(res.status_code, 200)
 
+
+class StudentSidebarLearningSettingsLinkTests(TestCase):
+    def test_student_sidebar_has_learning_settings_link(self):
+        student = User.objects.create(username="s", role="student")
+        self.client.force_login(student)
+        res = self.client.get(reverse("student_dashboard"))
+        self.assertEqual(res.status_code, 200)
+        html = res.content.decode("utf-8")
+        self.assertIn(reverse("student_learning_settings"), html)
